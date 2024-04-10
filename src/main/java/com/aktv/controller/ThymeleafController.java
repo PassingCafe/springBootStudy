cbf0a0910d7422607abd67a31e364017a08c8e73
王波 <307231598@qq.com>
2024-04-10 14:17:06 +0800

    添加日志功能和swagger2文档功能

--- src/main/java/com/aktv/controller/ThymeleafController.java
+++ src/main/java/com/aktv/controller/ThymeleafController.java
@@ -1,19 +1,30 @@
 package com.aktv.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/thymeleaf")
+@Slf4j
 public class ThymeleafController {
     @RequestMapping("/test404")
     public String test404() {
-    return "index";
+        return "index";
     }
+
     @RequestMapping("/test500")
     public String test500() {
         int i = 1 / 0;
         return "index";
+    }
+
+    @RequestMapping("/testlog")
+    @ResponseBody
+    public String testlog() {
+        log.info("======测试日志info级别打印=====");
+        return "success";
     }
 
 }
